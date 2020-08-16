a5454eb19115d384d5a016f9979555167ad6ef9e
kgtrey1 <kevin.gouyet@epitech.eu>
2020-08-16 12:29:54 +0200

    Adding manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Altera Framework
+-- Created by kgtrey1
+
+fx_version      'adamant'
+game            'gta5'
+description     'Altera Framework'
+version         '0.0.1'
+
+server_scripts {
+  'src/server/main.lua',
+}
