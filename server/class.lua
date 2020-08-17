d4048bcc43f0e475807a1599828747beb732cba8
kgtrey1 <kevin.gouyet@epitech.eu>
2020-08-17 18:39:17 +0200

    Added callback implementation (server side)

---
+++ server/class.lua
@@ -0,0 +1,35 @@
+-- Altera Framework - 2020
+-- Made by kgtrey1
+-- server/class.lua
+-- Object that will be shared between scripts.
+
+ARP = {}
+
+--
+-- Callbacks (server side implementation)
+--
+
+ARP.ServerCallbacks = {} -- List of the callbacks
+
+ARP.RegisterServerCallback = function(callbackName, functionPointer)
+    print("registering callback")
+    ARP.ServerCallbacks[callbackName] = functionPointer
+end
+
+ARP.TriggerServerCallback = function(callbackName, requestId, source, functionPointer, ...)
+    if (ARP.ServerCallbacks[callbackName] ~= nil) then
+        ARP.ServerCallbacks[callbackName](source, functionPointer, ...)
+    else
+        print(string.format("arp_framework: Cannot find callback '%s'.", callbackName))
+    end
+end
+
+RegisterServerEvent('arp:TriggerServerCallback')
+AddEventHandler('arp:TriggerServerCallback', function(callbackName, requestId, ...)
+    local _source = source
+
+    print("trigered")
+    ARP.TriggerServerCallback(callbackName, requestId, _source, function(...)
+        TriggerClientEvent('arp:ServerCallback', _source, requestId, ...)
+    end, ...)
+end)
