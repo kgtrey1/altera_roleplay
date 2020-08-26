fa90a18d8a1f2a36324bc672160a0f35f0018d8c
kgtrey1 <kevin.gouyet@epitech.eu>
2020-08-26 17:04:49 +0200

    Initial commit

---
+++ client/main.lua
@@ -0,0 +1,6 @@
+--
+-- ALTERA PROJECT, 2020
+-- ARP_personnalUI
+-- File description:
+-- Client side script
+--
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+--
+-- ALTERA PROJECT, 2020
+-- ARP_personnalUI
+-- File description:
+-- FX Manifest.
+--
+
+fx_version      'bodacious'
+game            'gta5'
+description     'Altera Framework - Personnal UIs'
+version         '0.0.5'
+dependency      'arp_framework'
+client_script   'client/main.lua'
+server_script   'server/main.lua'
