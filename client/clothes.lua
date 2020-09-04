69ccb8dfcf740f9f26fa2981c236f20661bca79b
kgtrey1 <kevin.gouyet@epitech.eu>
2020-09-04 22:22:32 +0200

    Rework of main menu

--- client/clothes.lua
+++ client/clothes.lua
@@ -13,56 +13,127 @@ ClothMenu       = {
     outfit  = 'arp_stylizer:ClothMenuOutfit'
 }
 
+NewOutfit   = {}
+
+function ItemIsOwned(compName, compOffset)
+    for k, v in pairs(Owned) do
+        if (k == compName) then
+            for i = 1, #v, 1 do
+                if (v[i] == compOffset) then
+                    return (true)
+                end
+            end
+            break
+        end
+    end
+    return (false)
+end
+
+local function OnItemSelected(name, offset, price, fakeOffset)
+    print(name)
+    print(offset)
+    print(price)
+    print(fakeOffset)
+    if (price == nil or ItemIsOwned(name, offset)) then
+        NewOutfit[name] = offset
+    else
+        ARP.TriggerServerCallback('arp_stylizer:BuyItem', function(resp, data)
+            if (resp == true) then
+                Clothes[name].ListF[fakeOffset] = 'OWNED'
+                NewOutfit[name] = offset
+                Owned = data.owned
+                print('its true')
+            end
+            
+        end, name, offset, price) 
+    end
+end
+
+local function DrawNonRestrictedList(v)
+    ARP.Menu.Item.List(v.Component.label, v.List, 1, '', {}, true, {
+        onListChange = function(index, item)
+            v.Texture.iterator = index
+            TriggerEvent('skinchanger:change', v.Component.name, index)
+        end, onSelected = function(index, item)
+            OnItemSelected(v.Component.name, index)
+        end
+    })
+    if (v.textured) then
+        ARP.Menu.Item.List(v.Texture.name, v.Texture.list[v.Texture.iterator], 1, '', {}, true, {
+            onListChange = function(index, item)
+                TriggerEvent('skinchanger:change', v.Texture.name, index - 1)
+            end, onSelected = function(index, item)
+                OnItemSelected(v.Texture.name, index - 1)
+            end
+        })
+    end
+end
+
+local function DrawRestrictedList(v)
+    ARP.Menu.Item.List(v.Component.label, v.ListF, 1, '', {}, true, {
+        onListChange = function(index, item)
+            v.Texture.iterator = index
+            TriggerEvent('skinchanger:change', v.Component.name, tonumber(v.ListR[index]))
+        end, onSelected = function(index, item)
+            OnItemSelected(v.Component.name, tonumber(v.ListR[index]), v.prices[tonumber(v.ListR[index])], index)
+        end
+    })
+    if (v.textured) then
+        ARP.Menu.Item.List(v.Texture.name, v.Texture.list[v.Texture.iterator], 1, '', {}, true, {
+            onListChange = function(index, item)
+                TriggerEvent('skinchanger:change', v.Texture.name, index - 1)
+            end, onSelected = function(index, item)
+                OnItemSelected(v.Texture.name, index - 1)
+            end
+        })
+    end
+end
+
+
 local function DrawBuyMenu()
-    for i = 1, #Clothes, 1 do
-        if (Clothes[i].restricted) then
-            ARP.Menu.Item.List(Clothes[i].Component.label, Clothes[i].ListF, 1, '', {}, true, {
-                onListChange = function(index, item)
-                    TriggerEvent('skinchanger:change', Clothes[i].Component.name, Clothes[i].ListR[index])
-                end, onSelected = function(index, item)
-                    print(string.format("real offset: %d, price: %d", Clothes[i].ListR[index], Clothes[i].prices[index]))
-                end
-            })
+    for k, v in pairs(Clothes) do
+        if (v.restricted) then
+            DrawRestrictedList(v)
         else
-            ARP.Menu.Item.List(Clothes[i].Component.label, Clothes[i].List, 1, '', {}, true, {
-                onListChange = function(index, item)
-                    TriggerEvent('skinchanger:change', Clothes[i].Component.name, index)
-                end, onSelected = function(index, item)
-                    
-                end
-            })
+            DrawNonRestrictedList(v)
         end
     end
 end
 
 function DrawClothesMenu()
+    if (ClothMenuIsOpen) then
+        return
+    end
     ARP.Menu.CloseAll()
-    ARP.Menu.Visible(ClothMenu.main, false)
+    ARP.Menu.Visible(ClothMenu.buy, false)
     ClothMenuIsOpen = true
     while (ClothMenuIsOpen) do
         Citizen.Wait(0)
         ARP.Menu.IsVisible(ClothMenu.buy, function()
-            DrawBuyMenu()
-		end, function() end, true)
-        ARP.Menu.IsVisible(ClothMenu.main, function()
-            ARP.Menu.Item.Button('Acheter des vêtements', '', {}, true, {}, ClothMenu.buy)
-            --ARP.Menu.Item.Button('Sauvegarder la tenue', '', {}, true, {}, ClothMenu.save)
-            --ARP.Menu.Item.Button('Voir les tenues sauvegardées', '', {}, true, {}, ClothMenu.outfit)
-		end, function() end, false)
+            for k, v in pairs(Clothes) do
+                if (v.restricted) then
+                    DrawRestrictedList(v)
+                else
+                    DrawNonRestrictedList(v)
+                end
+            end
+        end, function() end, false)
+        if (not ARP.Menu.GetVisibility(ClothMenu.buy, false)) then
+            ClothMenuIsOpen = false
+        end
     end
-    ClothMenuIsOpen = false
 end
 
 function RegisterClothMenus()
-    ARP.Menu.RegisterMenu(ClothMenu.main, 'Vetements', "MENU D'INTERACTION")
-    ARP.Menu.RegisterSubmenu(ClothMenu.buy, ClothMenu.main, 'Vetements', "MENU D'ACHAT")
-    ARP.Menu.RegisterSubmenu(ClothMenu.save, ClothMenu.main, 'Vetements', "MENU DE TENUES")
-    ARP.Menu.RegisterSubmenu(ClothMenu.outfit, ClothMenu.main, 'Vetements', "MENU DE TENUES")
+    ARP.Menu.RegisterMenu(ClothMenu.buy, 'Vetements', "MENU D'INTERACTION")
 end
 
 Citizen.CreateThread(function()
-    while (true) do
-        Citizen.Wait(0)
-        
-    end
+	while (true) do
+		Citizen.Wait(0)
+        if (IsControlJustPressed(1, 170)) then
+            Action = 'clothes'
+			TriggerEvent('skinchanger:getData', LoadSkinData)
+		end
+	end
 end)
