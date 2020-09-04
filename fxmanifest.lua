8381d71bd95f363d8e8ec8fc825a7861b195f761
kgtrey1 <kevin.gouyet@epitech.eu>
2020-09-04 22:23:05 +0200

    Up2date manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,9 +9,18 @@ fx_version      'bodacious'
 game            'gta5'
 description     'Altera personnalisation script'
 version         '0.0.1'
-dependency      'arp_framework'
+dependency      {
+    'arp_framework',
+    'skinchanger'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'server/main.lua'
+}
 
 client_script {
     'config.lua',
-    'client/main.lua'
+    'client/main.lua',
+    'client/clothes.lua'
 }
