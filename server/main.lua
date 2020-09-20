3e8307ed99d8242b828b0484d415eb40fba27fa3
kgtrey1 <kevin.gouyet@epitech.eu>
2020-09-20 17:01:06 +0200

    Adding key pickup on the server side

--- server/main.lua
+++ server/main.lua
@@ -5,8 +5,29 @@
 -- Main server script.
 --
 
-ARP = nil
+ARP  = nil
+Keys = {}
 
 TriggerEvent('arp_framework:FetchObject', function(obj)
     ARP = obj
 end)
+
+function PickupKeys(plate)
+    local _source   = source
+    local Alter     = ARP.GetPlayerById(_source)
+    local steamId   = Alter.GetSteamid()
+
+    if (Keys[plate] ~= nil and Keys[plate] == steamId) then
+        Keys[plate] == nil
+        Alter.ShowNotification(_U('drop_keys'))
+    elseif (Keys[plate] == nil) then
+        Keys[plate] == steamId
+        Alter.ShowNotification(_U('pickup_keys'))
+    else
+        Alter.ShowNotification(_U('keys_taken'))
+    end
+    return
+end
+
+RegisterNetEvent('arp_vehiclemanager:PickupKeys')
+AddEventHandler('arp_vehiclemanager:PickupKeys', PickupKeys)
