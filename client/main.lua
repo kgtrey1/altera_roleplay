bbef74b2de37ef1f6ccf5f5beee74ba5c96d1f52
kgtrey1 <kevin.gouyet@epitech.eu>
2020-09-20 16:47:07 +0200

    Adding main thread

--- client/main.lua
+++ client/main.lua
@@ -13,4 +13,19 @@ end)
 
 AddEventHandler('arp_framework:PlayerReady', function(data)
     ARP.Player = data
+    Citizen.CreateThread(ListenUserInput)
 end)
+
+function ListenUserInput()
+    while (true) do
+        Citizen.Wait(0)
+        if (IsControlJustPressed(1, 303)) then 
+            if (IsPedInAnyVehicle(PlayerPedId(), true)) then
+                manageKeyPicking()
+            else
+                changeVehicleState()
+            end
+            Citizen.Wait(3000)
+        end
+    end
+end
