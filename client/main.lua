c5c4001776285fe2748441c910405b01eb4f4251
kgtrey1 <kevin.gouyet@epitech.eu>
2020-09-20 18:06:04 +0200

    Adding function to close and open vehicle

--- client/main.lua
+++ client/main.lua
@@ -13,8 +13,36 @@ end)
 
 AddEventHandler('arp_framework:PlayerReady', function(data)
     ARP.Player = data
+    ARP.Streaming.RequestAnimDict(Config.Dict)
     Citizen.CreateThread(ListenUserInput)
 end)
+
+function ChangeVehicleState()
+    local vehicle, distance = ARP.World.GetClosestVehicle()
+    local plate = GetVehicleNumberPlateText(vehicle)
+    local state = GetVehicleDoorLockStatus(vehicle)
+
+    if (vehicle == nil or distance > Config.MaxDistance or not IsPlateValid()) then
+        ARP.ShowNotification("~r~Aucun véhicule à proximité~s~")
+        return
+    end
+    ARP.TriggerServerEvent('arp_vehiclemanager:DoesPlayerHaveKey', function(resp)
+        if (resp == false) then
+            ARP.ShowNotification("Vous n'avez pas les ~r~clés~s~ de ce véhicule.")
+            return
+        elseif (state == 1) then
+            ARP.ShowNotification("Vous avez ~r~fermé~s~ le vehicule.")
+            TaskPlayAnim(PlayerPedId(), Config.Dict, Config.Anim, 8.0, 8.0, -1, 48, 1, false, false, false)
+            SetVehicleDoorsLocked(vehicle, 2)
+            PlayVehicleDoorCloseSound(vehicle, 1)
+        else
+            ARP.ShowNotification("Vous avez ~g~ouvert~s~ le véhicule")
+            TaskPlayAnim(PlayerPedId(), Config.Dict, Config.Anim, 8.0, 8.0, -1, 48, 1, false, false, false)
+            SetVehicleDoorsLocked(vehicle, 1)
+			PlayVehicleDoorOpenSound(vehicle, 3)
+        end
+    end, plate)
+end
 
 function IsPlateValid(plate)
     return (true)
@@ -26,7 +54,7 @@ function PickupAndDropKeys()
     if (IsPlateValid(plate) == true) then
         TriggerServerEvent('arp_vehiclelock:PickupKeys', plate)
     else
-        ARP.ShowNotification(_U('keys_taken'))
+        ARP.ShowNotification("~r~Il n'y a pas de clés dans ce véhicule~s~")
     end
     return
 end
