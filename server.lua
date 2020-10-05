09b6ddaa8114fac7481e7b7f4ace4ac4e7c029aa
kgtrey1 <kevin.gouyet@epitech.eu>
2020-10-05 21:12:00 +0200

    Complete recode of the serverside script

--- server.lua
+++ server.lua
@@ -1,45 +1,116 @@
-local ESX = nil
--- ESX
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+local ARP = nil
 
--- Open ID card
-RegisterServerEvent('jsfour-idcard:open')
-AddEventHandler('jsfour-idcard:open', function(ID, targetID, type)
-	local identifier = ESX.GetPlayerFromId(ID).identifier
-	local _source 	 = ESX.GetPlayerFromId(targetID).source
-	local show       = false
-
-	MySQL.Async.fetchAll('SELECT firstname, lastname, dateofbirth, sex, height FROM users WHERE identifier = @identifier', {['@identifier'] = identifier},
-	function (user)
-		if (user[1] ~= nil) then
-			MySQL.Async.fetchAll('SELECT type FROM user_licenses WHERE owner = @identifier', {['@identifier'] = identifier},
-			function (licenses)
-				if type ~= nil then
-					for i=1, #licenses, 1 do
-						if type == 'driver' then
-							if licenses[i].type == 'drive' or licenses[i].type == 'drive_bike' or licenses[i].type == 'drive_truck' then
-								show = true
-							end
-						elseif type =='weapon' then
-							if licenses[i].type == 'weapon' then
-								show = true
-							end
-						end
-					end
-				else
-					show = true
-				end
-
-				if show then
-					local array = {
-						user = user,
-						licenses = licenses
-					}
-					TriggerClientEvent('jsfour-idcard:open', _source, array, type)
-				else
-					TriggerClientEvent('esx:showNotification', _source, "You don't have that type of license..")
-				end
-			end)
-		end
-	end)
+TriggerEvent('arp_framework:FetchObject', function(obj)
+	ARP = obj
 end)
+
+local function GetFirearmsInfos(Alter)
+	local firearmsLicense = Alter.licenses.GetFirearmsLicense()
+
+	if (firearmsLicense) then
+		local userData = Alter.GetIdentity()
+
+		return ({
+			firstname	= userData.firstName,
+			lastname 	= userData.lastName,
+			dateOfBirth = userData.dateOfBirth,
+			gender 		= userData.gender,
+			height 		= userData.height,
+			firearms	= true
+		}, true)
+	end
+	return (nil, false)
+end
+
+local function GetDrivingInfos(Alter)
+	local drivingLicense = Alter.licenses.GetDrivingLicense()
+
+	if (drivingLicense.car or drivingLicense.bike or drivingLicense.truck) then
+		local userData = Alter.GetIdentity()
+
+		return ({
+			firstname	= userData.firstName,
+			lastname 	= userData.lastName,
+			dateOfBirth = userData.dateOfBirth,
+			gender 		= userData.gender,
+			height 		= userData.height,
+			car			= drivingLicense.car,
+			truck		= drivingLicense.truck,
+			bike		= drivingLicense.bike
+		}, true)
+	end
+	return (nil, false)
+end
+
+local function GetIdentityInfos(Alter)
+	if (Alter.licenses.GetIdCard()) then
+		local userData = Alter.GetIdentity()
+
+		return ({
+			firstname	= userData.firstName,
+			lastname 	= userData.lastName,
+			dateOfBirth = userData.dateOfBirth,
+			gender 		= userData.gender,
+			height 		= userData.height
+		}, true)
+	end
+	return (nil, false)
+end
+
+function ShowLicense(target, type)
+	local _source 	= source
+	local Altera	= ARP.GetPlayerById(_source)
+	local Alterb	= ARP.GetPlayerById(target)
+	local show		= false
+	local data		= nil
+
+	if (type == "id" or type == "driving" or type == "firearms") then
+		if (type == 'id') then
+			data, show = GetIdentityInfos(Alterb)
+		elseif (type == 'driving') then
+			data, show = GetDrivingInfos(Alterb)
+		else
+			data, show = GetFirearmsInfos(Alterb)
+		end
+		if (show) then
+			Altera.TriggerEvent('arp_idcard:ShowLicense', data, type)
+		else
+			Altera.ShowNotification("Cette personne ne possède pas ce type de license.")
+		end
+	else
+		print("Illegal call")
+	end
+	return
+end
+
+RegisterServerEvent('arp_idcard:RequestLicense')
+AddEventHandler('arp_idcard:RequestLicense', RequestLicense)
+
+function ShowLicense(target, type)
+	local _source 	= source
+	local Altera	= ARP.GetPlayerById(_source)
+	local Alterb	= ARP.GetPlayerById(target)
+	local show		= false
+	local data		= nil
+
+	if (type == "id" or type == "driving" or type == "firearms") then
+		if (type == 'id') then
+			data, show = GetIdentityInfos(Altera)
+		elseif (type == 'driving') then
+			data, show = GetDrivingInfos(Altera)
+		else
+			data, show = GetFirearmsInfos(Altera)
+		end
+		if (show) then
+			Alterb.TriggerEvent('arp_idcard:ShowLicense', data, type)
+		else
+			Altera.ShowNotification("Vous ne possedez pas ce type de licence.")
+		end
+	else
+		print("Illegal call")
+	end
+	return
+end
+
+RegisterServerEvent('arp_idcard:ShowLicense')
+AddEventHandler('arp_idcard:ShowLicense', ShowLicense)
