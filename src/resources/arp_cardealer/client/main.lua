fe1952e89e073ebfc73ccaddc4439fe5e9326184
kgtrey1 <kevin.gouyet@epitech.eu>
2020-12-03 16:51:52 +0100

    Adding basic configuration for all jobs

--- src/resources/arp_cardealer/client/main.lua
+++ src/resources/arp_cardealer/client/main.lua
@@ -4,3 +4,18 @@
 -- File description:
 -- main client side script
 --
+
+ARP = nil
+Blips = {}
+
+TriggerEvent('arp_framework:FetchObject', function(Obj)
+	ARP = Obj
+end)
+
+AddEventHandler('arp_framework:PlayerReady', function(playerData)
+    ARP.Player = playerData
+    
+    for _, enterprise in pairs(Config.Enterprises) do -- Register enterprise, either for sale or for blip.
+        TriggerEvent('arp_enterprise:RegisterEnterpriseStatus', {name = enterprise.name, bpos = enterprise.shared.bpos, sale = enterprise.shared.sale, blipsetting = Config.Blip})
+    end
+end)
--- src/resources/arp_cardealer/config.lua
+++ src/resources/arp_cardealer/config.lua
@@ -4,3 +4,28 @@
 -- File description:
 -- Configuration file
 --
+
+Config = {}
+
+Config.Blip = {
+    color   = 0,
+    sprite  = 0,
+    text    = "Concessionnaire"
+}
+
+Config.Enterprises = {
+    {
+        name = 'concess1',
+        shared = {
+            sale = vector3(1155.12, 2248.14, 49.1),
+            bpos = vector3(100, 100, 100)
+        }
+    },
+    {
+        name = 'concess2',
+        shared = {
+            sale = vector3(-100, -100, -100),
+            bpos = vector3(100, 100, 100)
+        }     
+    }
+}
