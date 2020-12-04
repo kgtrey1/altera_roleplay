99326238b1c0904f1f34b08099e9a6fcd32106ea
kgtrey1 <kevin.gouyet@epitech.eu>
2020-12-04 17:49:50 +0100

    Adding sorting algorythm for catalogs

--- src/resources/arp_cardealer/server/main.lua
+++ src/resources/arp_cardealer/server/main.lua
@@ -4,3 +4,82 @@
 -- File description:
 -- main server script
 --
+
+ENT = {}
+Car = {}
+Cat = {}
+
+function AssignCarToEnterprises()
+    local list  = nil
+    local found = false
+
+    for name, _ in pairs(ENT) do
+        for __, vehicle in pairs(Car) do
+            found = false
+            if (vehicle.owned_by_default) then
+                ENT[name].owned[vehicle.category][vehicle.model] = vehicle
+            else
+                for ___, ent in pairs(vehicle.owned_by) do
+                    if (ent == name) then
+                        ENT[name].owned[vehicle.category][vehicle.model] = vehicle
+                        found = true
+                        break
+                    end
+                end
+                if (not found) then
+                    ENT[name].not_owned[vehicle.category][vehicle.model] = vehicle
+                end
+            end
+        end
+    end
+end
+
+function AssignCategoriesToEnterprises()
+    for name, _ in pairs(ENT) do
+        ENT[name].owned     = {}
+        ENT[name].not_owned = {}
+        for cat, _ in pairs(Cat) do
+            ENT[name].owned[cat]     = {}
+            ENT[name].not_owned[cat] = {}
+        end
+    end
+end
+
+function PopulateCategoryTable(categories)
+    print('ARP_cardealer> Populating tables for all categories.')
+    for _, categorie in ipairs(categories) do
+        Cat[categorie.name] = categorie
+    end
+    return
+end
+
+function PopulateCarTable(cars)
+    print('ARP_cardealer> Populating tables for all cars.')
+    for _, car in ipairs(cars) do
+        Car[car.name] = car
+        Car[car.name].owned_by = json.decode(Car[car.name].owned_by)
+    end
+    return
+end
+
+function PopulateEnterpriseTable(cardealers)
+    print('ARP_Cardealer> Populating tables for all enterprises.')
+    for _, cardealer in ipairs(cardealers) do
+        ENT[cardealer.name] = {}
+    end
+    return
+end
+
+MySQL.ready(function()
+	local cardealers    = MySQL.Sync.fetchAll('SELECT name from enterprises WHERE jobname = @job', {
+        ['@job'] = 'cardealer'
+    })
+    local cars          = MySQL.Sync.fetchAll('SELECT * from cardealer_catalog', {})
+    local categories    = MySQL.Sync.fetchAll('SELECT * from cardealer_category', {})
+
+    PopulateEnterpriseTable(cardealers)
+    PopulateCarTable(cars)
+    PopulateCategoryTable(categories)
+    AssignCategoriesToEnterprises()
+    AssignCarToEnterprises()
+end)
